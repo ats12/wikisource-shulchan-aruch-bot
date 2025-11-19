1181307b22182b25fc3b5fe859a89e7b765e1c51
atsdev <amitaits384@gmail.com>
2025-11-19 17:31:32 +0200

    Added the actual editing functionality, and splitted the data to another file.

--- shulchan_aruch_bot.py
+++ shulchan_aruch_bot.py
@@ -3,9 +3,8 @@ from pywikibot import pagegenerators
 from pywikibot import textlib
 import wikitextparser as wtp
 import re
+from conversion_data import *
 site = pywikibot.Site("he", "wikisource")
-
-commenter_shortcuts = {"משנה ברורה": "מב", "פתחי תשובה": "פת", "באר היטב": "בהט", "מגן אברהם": "מא", "ביאור הלכה": "בהל", "טורי זהב": "טז", "ט\"ז": "טז", "כף החיים": "כהח", "באר הגולה": "בהג", "פרי מגדים": "פרמג"}
 
 def construct_commenter(section, commenter):
     section = section[11:]
@@ -37,7 +36,7 @@ def parse_completion_table(section):
 
 def get_paragraphs(commenter_page):
     parsed_commenter = wtp.parse(commenter_page.text)
-    paragraphs = [(template.get_arg("2"), template.get_arg("3")) for template in parsed_commenter.templates if template.normal_name() == "משע"]
+    paragraphs = [(template.get_arg("2"), template.get_arg("3").value) for template in parsed_commenter.templates if template.normal_name() == "משע"]
     return paragraphs
 
 def edit_section(section_tuple: tuple):
@@ -47,10 +46,19 @@ def edit_section(section_tuple: tuple):
     commenter = section_tuple[1]
     section_page = pywikibot.Page(site, section)
     commenter_page = pywikibot.Page(site, construct_commenter(section, commenter))
-    if not commenter_page.exists(): return False
+    if not commenter_page.exists(): return None
     paragraphs = get_paragraphs(commenter_page)
-    print(paragraphs)
-    refs = [f"{{{{פרשע1|{commenter_shortcuts[commenter]}|{paragraph[0]}}}}}" for paragraph in paragraphs]
+    refs = [(paragraph[1], f"{{{{פרשע1|{commenter_shortcuts[commenter]}|{paragraph[0]}}}}}") for paragraph in paragraphs]
+    print(section_page.text)
+    for ref in refs:
+        heading = re.search(heading_formats[commenter], ref[0]).group(1)
+        insert_pos = re.search(heading, section_page.text)
+        if insert_pos:
+            insert_pos = insert_pos.start()
+        else: continue
+        section_page.text = section_page.text[:insert_pos] + ref[1] + section_page.text[insert_pos:]
+    section_page.save(f"הוספת הפניות ל{commenter}.")
+
 
 sections = ["אורח חיים", "יורה דעה", "אבן העזר", "חושן משפט"]
 to_edit = []
