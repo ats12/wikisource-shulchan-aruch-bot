c16d0c8eea042cae64e6b84ada49fb9f3985363c
atsdev <amitaits384@gmail.com>
2025-11-21 11:37:55 +0200

    added partial edit functionality

--- shulchan_aruch_bot.py
+++ shulchan_aruch_bot.py
@@ -1,6 +1,5 @@
 import pywikibot
-from pywikibot import pagegenerators
-from pywikibot import textlib
+from pywikibot import add_text
 import wikitextparser as wtp
 import re
 import sys # for printing to stderr
@@ -61,7 +60,7 @@ def create_row(data):
     row += data[-1]
     return row
 
-def edit_completion_table(section_tuple, table_text):
+def edit_completion_table(section_tuple, table_text, mark):
     parsed = wtp.parse(table_text)
     title_row = parsed.tables[0].data(row=0)
     for table in parsed.tables:
@@ -69,12 +68,12 @@ def edit_completion_table(section_tuple, table_text):
         for i, row in enumerate(table_data):
             if row[0][2:-2] == section_tuple[0]:
                 new_row = row.copy()
-                new_row[title_row.index(section_tuple[1])] = "{{v}}"
+                new_row[title_row.index(section_tuple[1])] = mark
                 table_text = table_text.replace(create_row(row), create_row(new_row))
                 return table_text
 
-def update_completion_table(sections):
-    section = " ".join(section_tuple[0].split()[2:5])
+def update_completion_table(sections, mark):
+    section = " ".join(sections[0][0].split()[2:5])
     completion_table = get_completion_table(" ".join(section.split()[:-1]))
     for section in sections:
         completion_table.text = edit_completion_table(section, completion_table.text)
@@ -105,14 +104,24 @@ def edit_section(section_tuple: tuple):
     paragraphs = get_paragraphs(commenter_page)
     refs = [(paragraph[1], f"{{{{פרשע1|{commenter_shortcuts[commenter]}|{paragraph[0]}}}}}") for paragraph in paragraphs]
     if not refs: return -2
+    not_done = []
     for ref in refs:
         if section_page.text.find(ref[1]): continue # if the reference is already found in the page, don't re-add it
         heading = re.search(heading_formats[commenter], ref[0]).group(1)
         insert_pos = re.search(heading, section_page.text)
         if insert_pos:
             insert_pos = insert_pos.start()
-        else: continue
+        else:
+            not_done.append(ref[0])
         section_page.text = section_page.text[:insert_pos] + ref[1] + section_page.text[insert_pos:]
+    if not_done == refs: return -3
+    if not_done:
+        message = f"\n=== הוספת הפניות ל{commenter} ===\nהוספו הפניות ל{commenter} באמצעות בוט. הסעיפים הקטנים הבאים לא הושלמו: {", ".join(not_done)}. ~~~~"
+        section_page.save(f"הוספת הפניות חלקית, ראו פרטים נוספים בדף השיחה")
+        discussion_page = pywikibot.Page(site, "שיחה:" + section)
+        discussion_page.text += message
+        discussion_page.save()
+        return -4
     section_page.save(f"הוספת הפניות ל{commenter}.")
     return True
 
@@ -123,14 +132,27 @@ for section in sections:
     to_edit += parse_completion_table(section)
 
 done = []
-for section in_to edit:
-    match edit_section(section):
+partially_done = []
+for section in to_edit:
+    try:
+        edit_status = edit_section(section)
+    except Exception as error:
+            print(error)
+
+    match edit_status:
         case -1:
             print(f"דף המפרש {construct_commenter(section[0], section[1])} אינו קיים", file=sys.stderr)
         case -2:
-            print(f"לא נמצאו תבניות {{משע}} בדף המפרש {construct_commenter(section[0], section[1])}", file=sys.stderr)
+            print(f"לא נמצאו תבניות {{{{משע}}}} בדף המפרש {construct_commenter(section[0], section[1])}", file=sys.stderr)
+        case -3:
+            print(f"הבוט לא הצליח לזהות את הדיבורים המתחילים של {section[1]} ב{section[0]}")
+        case -4:
+            partially_done.append(section)
+            print(f"הפניות ל{section[1]} נוספו בהצלחה חלקית ל{section[0]}")
         case True:
             done.append(section)
-            print(f"הפניות ל{section[1]} נוספו בהצלחה ל{section[0}")
+            print(f"הפניות ל{section[1]} נוספו בהצלחה ל{section[0]}")
+            break
 
-update_completion_table(done)
+update_completion_table(done, "{{v}}")
+update_completion_table(partially_done, "{{v}}{{הערה|שם=השלמה חלקית בוט}}")
